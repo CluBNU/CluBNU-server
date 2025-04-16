e91338b9e8e764f6d39ef6a0f718f077c0fb671f
sso9594 <sso9594@naver.com>
2025-04-16 22:05:00 +0900

    test: OAuth OIDC 테스트 코드

---
+++ clubnu-domain/domain-rds/src/testFixtures/java/com/clubnu/fixtures/UserFixture.java
@@ -0,0 +1,9 @@
+package com.clubnu.fixtures;
+
+import com.clubnu.domain.user.entity.User;
+
+public class UserFixture {
+    public static User user(){
+        return new User();
+    }
+}
---
+++ clubnu-domain/domain-service/src/test/java/com/clubnu/domain/oauth/OAuthDomainServiceTest.java
@@ -0,0 +1,11 @@
+package com.clubnu.domain.oauth;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class OAuthDomainServiceTest {
+
+}
