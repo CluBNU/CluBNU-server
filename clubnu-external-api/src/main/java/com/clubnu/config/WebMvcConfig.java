7c77c635c42c7bf190b1c2726a934033023be8fe
sso9594 <sso9594@naver.com>
2025-05-08 13:49:16 +0900

    feat: cors 허용 (임시)

--- clubnu-external-api/src/main/java/com/clubnu/config/WebMvcConfig.java
+++ clubnu-external-api/src/main/java/com/clubnu/config/WebMvcConfig.java
@@ -15,7 +15,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8082")
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowCredentials(true);
